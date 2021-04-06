8a1559f82572e3875d2ac9fadbccb2ab2980dfd6
mohdgazyanii235 <mohdgazyanii235@users.noreply.github.com>
2021-04-06 23:46:16 +0100

    whitenoise

--- phishing/settings.py
+++ phishing/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
