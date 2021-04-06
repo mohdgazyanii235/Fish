874d6a3aea0b7da54d5c3132147cc3191d04f732
mohdgazyanii235 <mohdgazyanii235@users.noreply.github.com>
2021-04-06 23:12:18 +0100

    added procfile and requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn phishing.wsgi
--- phishing/settings.py
+++ phishing/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'root',
-        'HOST': '192.168.0.184',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
